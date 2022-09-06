f07bd8a3d0ad244215e62a39effbd5f12ae14353
sreenu51 <sreenuannam51@gmail.com>
2022-09-06 18:15:08 +0530

    changes updated

---
+++ Terraform Commands.tf
@@ -0,0 +1,8 @@
+1. terraform --version   - To verify the terraform version 
+2. tfswitch  - To switch version from one version to another version 
+3. terraform init - initilize the working directory 
+4. terraform validate - validate the configuration files
+5. terraform plan - preview the configuration changes
+6. aws config - configure aws account
+7. terraform apply  - apply the planned changes 
+8.
--- syntax.tf
+++ syntax.tf
@@ -15,3 +15,51 @@ terraform {
 provider "aws" {
     region = "us-east-1"
 }
+
+# Variable decleration
+
+variable "vpcname" {
+    type = string
+    default = "MyVpc"
+  
+}
+
+variable "PortNumber" {
+    type = number 
+    default = 22
+  
+}
+
+variable "Mylist" {
+    type = list(sting)
+    default = [ "vlue1","value2" ]
+  
+}
+
+variable "Mymap" {
+    type = Mymap
+    default = {
+        key1 = "value1"
+        key2 = "value2"
+    }
+}
+
+variable "inputname" {
+    type = string
+    description = "set the name of the VPC"
+  
+}
+
+resource "aws_vpc" "MyVpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      "key1" = "value1"
+    }
+  
+}
+
+
+output "Myoutput" {
+    value = aws_vpc.MyVpc.id
+  
+}
